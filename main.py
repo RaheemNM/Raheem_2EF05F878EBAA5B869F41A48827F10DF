6f2889c23dd5fc9ef5718fedcb4c9c75b4f2d0ff
RaheemNM <144360825+RaheemNM@users.noreply.github.com>
2023-09-27 14:52:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,82 @@
- #1.1 Implement a recursive function to calculate the factorial of a given number.
+'''Implement a class called BankAccount that represents a bank account. The class should have private 
+attributes for account number, account holder name, and account balance. Include methods to 
+deposit money, withdraw money, and display the account balance. Ensure that the account balance 
+cannot be accessed directly from outside the class. Write a program to create an instance of the 
+BankAccount class and test the deposit and withdrawal functionality.'''
 
-def factorial(n):
-    # Base case: factorial of 0 is 1
-    if n == 0:
-        return 1
-    # Recursive case: factorial of n is n times factorial of (n-1)
-    else:
-        return n * factorial(n - 1)
 
-number = int(input("Enter number: "))
-result = factorial(number)
-print(f"The factorial of {number} is {result}")
+class BankAccount:
+
+    def __init__(self, account_number, account_holder_name, initial_balance):
+
+        self.__account_number = account_number
+
+        self.__account_holder_name = account_holder_name
+
+        self.__account_balance = initial_balance
+
+ 
+
+    def deposit(self, amount):
+
+        if amount > 0:
+
+            self.__account_balance += amount
+
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+
+        else:
+
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+ 
+
+    def withdraw(self, amount):
+
+        if amount > 0 and amount <= self.__account_balance:
+
+            self.__account_balance -= amount
+
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+
+        else:
+
+            print("Invalid withdrawal amount or insufficient balance.")
+
+ 
+
+    def display_balance(self):
+
+        print(f"Account Balance for {self.__account_holder_name} (Account #{self.__account_number}): ${self.__account_balance}")
+
+ 
+
+ 
+
+# Test the BankAccount class
+
+if __name__ == "__main__":
+
+    # Create an instance of BankAccount
+
+    my_account = BankAccount("123456789", "John Doe", 1000.0)
+
+ 
+
+    # Test deposit and display_balance
+
+    my_account.display_balance()
+
+    my_account.deposit(500.0)
+
+    my_account.display_balance()
+
+ 
+
+    # Test withdrawal and display_balance
+
+    my_account.withdraw(200.0)
+
+    my_account.display_balance()
+
+    my_account.withdraw(1500.0)  # This should result in an error message
